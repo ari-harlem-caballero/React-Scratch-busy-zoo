36d0863bd4c0e94cfa35a26753a8be14bf73a4f4
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-28 12:00:37 -0800

    added HTML elements

--- src/App.js
+++ src/App.js
@@ -9,9 +9,38 @@ function App() {
 
   return (
     <div className="App">
-      {/* fight */}
-      {/* sign */}
-      {/* parade */}
+      <section className='fight'>
+        <div className='crab'>
+          <p>🦀</p>
+          <button>Crab gains strength</button>
+          <button>Crab attacks Squid</button>
+        </div>
+        <div className='squid'>
+          <p>🦑</p>
+          <button>Squid gains strength</button>
+          <button>Squid attacks Crab</button>
+        </div>
+      </section>
+
+      <section className='sign'>
+        <div>
+          <button>Close Zoo</button>
+          <button>Open Zoo</button>
+        </div>
+      </section>
+
+      <section className='parade'>
+        <div>
+          <button>Add Hippo</button>
+          <button>Add Rhino</button>
+          <button>Add Hedgehog</button>
+          <button>Add Deer</button>
+          <button>Add Rabbit</button>
+          <button>Add Sheep</button>
+          <button>Add Llama</button>
+          <button>Add Skunk</button>
+        </div>
+      </section>
     </div>
   );
 }
